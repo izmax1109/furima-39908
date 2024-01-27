2bd18c3494b0738dbb57baf6ccf55e36358e9b12
izmax1109 <izuizu1109@gmail.com>
2024-01-27 16:02:33 +0900

    カテゴリーの選択実装とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  validates :image, :category, :condition, :payer, :region, :shipping_day, presence: true
+
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payer
+  belongs_to :region
+  belongs_to :shipping_day
+
+  validates :title, :text, presence: true
+  validates :category_id, :condition_id, :payer_id, :region_id, :shipping_day_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+
+  has_one_attached :image
 end
